a0b6d1bbdef71a82705fa1be120a46f32d6d4dee
BraanFR <vince.lagneau@gmail.com>
2022-02-15 18:12:29 +0100

    Ch4 Step3

--- web/component/app.js
+++ web/component/app.js
@@ -1,26 +1,72 @@
 
-const { displayAlert } = require('../script')
+// const { displayAlert } = require('../script')
 
-const btn = <button onClick={ () => displayAlert() }>Click Me</button>
-ReactDOM.render(
-    btn,
-    document.getElementById('root')
-);
+// require('!!file-loader?name=[name].[ext]!../index.html')
+// // require('../css/tuto.webflow.css');
+
+// const btn = <button onClick={ () => displayAlert() }>Click Me</button>
+// ReactDOM.render(
+//     btn,
+//     document.getElementById('root')
+// );
 
 
 
+// var createReactClass = require('create-react-class')
+
+// var Page = createReactClass({
+//   render(){
+//     return <JSXZ in="template" sel=".container">
+//       <Z sel=".item">Burgers</Z>,
+//       <Z sel=".price">50</Z>
+//     </JSXZ>
+//   }
+// })
+
+// ReactDOM.render(
+//   <Page/>,
+//   document.getElementById('burger')
+// )
+
+
+
+require('!!file-loader?name=[name].[ext]!../index.html')
+/* required library for our React app */
+var ReactDOM = require('react-dom')
+var React = require("react")
 var createReactClass = require('create-react-class')
+
+/* required css for our application */
+// require('../css/tuto.webflow.css');
 
 var Page = createReactClass({
   render(){
-    return <JSXZ in="template" sel=".container">
-      <Z sel=".item">Burgers</Z>,
-      <Z sel=".price">50</Z>
+    return <JSXZ in="orders" sel=".supcontainer">
     </JSXZ>
   }
 })
 
-ReactDOM.render(
-  <Page/>,
-  document.getElementById('burger')
-)
+ReactDOM.render(<Page/>, document.getElementById('root'));
+
+
+var orders = [
+  {remoteid: "000000189", custom: {customer: {full_name: "TOTO & CIE"}, billing_address: "Some where in the world"}, items: 2}, 
+  {remoteid: "000000190", custom: {customer: {full_name: "Looney Toons"}, billing_address: "The Warner Bros Company"}, items: 3}, 
+  {remoteid: "000000191", custom: {customer: {full_name: "Asterix & Obelix"}, billing_address: "Armorique"}, items: 29}, 
+  {remoteid: "000000192", custom: {customer: {full_name: "Lucky Luke"}, billing_address: "A Cowboy doesn't have an address. Sorry"}, items: 0}, 
+]
+
+
+var Table = createReactClass({
+  render(){
+    return orders.map(order => (<JSXZ in="orders" sel=".table-line">
+    <Z sel=".command-number">{order.remoteid}</Z>
+    <Z sel=".customer-name">{order.custom.customer.full_name}</Z>
+    <Z sel=".adress1">{order.custom.billing_address}</Z>
+    <Z sel=".quantity">{order.items}</Z>
+    </JSXZ>
+    ))
+  }
+})
+  
+  ReactDOM.render(<Table/>, document.getElementById('table-lines'));
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,20 @@
+var path = require('path')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+
 module.exports = {
   entry: './component/app.js',
-  output: { filename: 'bundle.js' },
-  plugins: [],
+  output: {
+    path: path.join(__dirname, './priv/static' ),
+    filename: 'bundle.js',
+  },
+  //This will bundle all our .css file inside styles.css
+  // optimization: {
+  //   splitChunks: {cacheGroups: {styles: {name: 'styles', test: /\.css$/ , chunks: 'all', enforce: true}}},
+  //   minimizer: [`...`, new CssMinimizerPlugin()]
+  // },
+  plugins: [new MiniCssExtractPlugin({insert: "", filename: "[name].css"})],
   module: {
     rules: [
       {
@@ -17,6 +30,10 @@ module.exports = {
           }
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.(css)$/,
+        use: [{loader: MiniCssExtractPlugin.loader}, { loader: "css-loader" }]
       }
     ]
   },
